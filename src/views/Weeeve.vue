dcde24b51ea89b17ebeed1313c98e3a2a7bff5d8
abhi <abhiramk@stud.ntnu.no>
2024-06-27 01:45:26 +0200

    test

--- src/views/Weeeve.vue
+++ src/views/Weeeve.vue
@@ -5,16 +5,20 @@ import {RouterLink} from "vue-router";
 
 <template>
   <nav>
-    <RouterLink to="/about">Om meg</RouterLink>
-    <RouterLink to="/projects">Prosjekter</RouterLink>
-    <a href="https://no.linkedin.com/in/abhirami-karunananthan-b81a5522a?original_referer=https%3A%2F%2Fwww.google.com%2F"
-       target="_blank" rel="noopener">
-      <img alt="LinkedIn logo" class="icon" src="@/assets/images/linkedin.svg" width="20" height="20"/>
+    <RouterLink to="/about">Om meg
+      <img alt="about_logo" class="about" src="@/assets/images/woman.svg" width="20" height="20" />
+    </RouterLink>
+    <RouterLink to="/projects">Prosjekter
+      <img alt="projects_logo" class="project" src="@/assets/images/projects.svg" width="15" height="15" />
+    </RouterLink>
+    <a href="https://no.linkedin.com/in/abhirami-karunananthan-b81a5522a?original_referer=https%3A%2F%2Fwww.google.com%2F" target="_blank" rel="noopener">
+      <span class="linkedin-text">LinkedIn </span>
+      <img alt="LinkedIn logo" class="icon" src="@/assets/images/linkedin.svg" width="10" height="10" />
     </a>
   </nav>
 
   <div class="weeeveinfo">
-    <img alt="Vue logo" class="image" src="@/assets/images/weeeve.png" width="485" height="200"/>
+    <img alt="Vue logo" class="image" src="@/assets/images/weeeve.png" width="490" height="200"/>
 
     <div class="info-box">
       <p> Vi brukte blant annet AI-verktøyet ML-kit av google for ansiktsgjennkjenning. Applikasjonen lot brukeren lage  <b class="bold"> egendefinerte GIFs ved å
@@ -32,7 +36,113 @@ import {RouterLink} from "vue-router";
 </template>
 
 <style scoped>
+@media (max-width: 1023px) {
+  .icon{
+    vertical-align: middle;
+  }
+.weeeveinfo{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  flex-direction: column; /* Stack items vertically */
+
+}
+  .project{
+    vertical-align: middle;
+    margin-top: 3px;
+  }
+  .about{
+    vertical-align: middle;
+    margin-top: 3px;
+  }
+  header {
+    line-height: 1.5;
+    max-height: 100vh;
+  }
+
+  .image {
+    border-radius: 10px;
+    margin-top: 70px;
+    margin-left: 54px;
+    align-self: center;
+  }
+
+
+
+  .info-box {
+    background-color: #ffffff;
+    border-radius: 10px;
+    margin: 10px;
+    padding: 10px;
+    align-content: center;
+    text-align: center;
+    justify-content: center;
+    width: 80%; /* Adjust width as needed */
+    max-width: 600px; /* Example: Set maximum width for the info box */
+
+  }
+
+  .buttonswhatever a {
+    flex: 1;
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #fed9ca;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    color: #fff;
+    text-decoration: none;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    margin: 15px;
+    font-size: 12px;
+    font-weight: bold;
+    align-content: center;
+    max-width: 126px;
+    text-align: center;
+  }
+
+  .buttonswhatever a:hover {
+    background-color: #b2968b;
+  }
+
+  .bold{
+    font-weight: bold;
+  }
+
+  nav {
+    width: 100%;
+    font-size: 12px;
+    text-align: end;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    background-color: white;
+
+  }
+
+  nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+  }
+
+  nav {
+    text-align: center;
+    margin-left: -1rem;
+    font-size: 20px;
+    justify-content: space-around;
+    display: flex;
+    padding: 1rem 0;
+  }
+}
 @media (min-width: 1024px) {
+  .project{
+    display: none;
+  }
+  .about{
+    display: none;
+  }
   header {
     line-height: 1.5;
     max-height: 100vh;
@@ -103,6 +213,9 @@ import {RouterLink} from "vue-router";
     font-size: 1rem;
     padding: 1rem 0;
     margin-top: 1rem;
+  }
+  .linkedin-text {
+    display: none;
   }
 }
 </style>
