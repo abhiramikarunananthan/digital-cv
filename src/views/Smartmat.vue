8535bbe0f6fc684d955721ab3f154252e526eef0
abhi <abhiramk@stud.ntnu.no>
2024-06-27 01:27:11 +0200

    test

--- src/views/Smartmat.vue
+++ src/views/Smartmat.vue
@@ -5,11 +5,15 @@ import {RouterLink} from "vue-router";
 
 <template>
   <nav>
-    <RouterLink to="/about">Om meg</RouterLink>
-    <RouterLink to="/projects">Prosjekter</RouterLink>
-    <a href="https://no.linkedin.com/in/abhirami-karunananthan-b81a5522a?original_referer=https%3A%2F%2Fwww.google.com%2F"
-       target="_blank" rel="noopener">
-      <img alt="LinkedIn logo" class="icon" src="@/assets/images/linkedin.svg" width="20" height="20"/>
+    <RouterLink to="/about">Om meg
+      <img alt="about_logo" class="about" src="@/assets/images/woman.svg" width="20" height="20" />
+    </RouterLink>
+    <RouterLink to="/projects">Prosjekter
+      <img alt="projects_logo" class="project" src="@/assets/images/projects.svg" width="15" height="15" />
+    </RouterLink>
+    <a href="https://no.linkedin.com/in/abhirami-karunananthan-b81a5522a?original_referer=https%3A%2F%2Fwww.google.com%2F" target="_blank" rel="noopener">
+      <span class="linkedin-text">LinkedIn </span>
+      <img alt="LinkedIn logo" class="icon" src="@/assets/images/linkedin.svg" width="10" height="10" />
     </a>
   </nav>
 
@@ -50,7 +54,113 @@ import {RouterLink} from "vue-router";
 </template>
 
 <style scoped>
-@media (min-width: 1000px) {
+@media (max-width: 1023px) {
+  .icon{
+    vertical-align: middle;
+  }
+
+  .project{
+    vertical-align: middle;
+    margin-top: 3px;
+  }
+  .about{
+    vertical-align: middle;
+    margin-top: 3px;
+  }
+  .videocontainer{
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content:center;
+    margin: 14px;
+  }
+  header {
+    line-height: 1.5;
+    max-height: 100vh;
+  }
+
+  video {
+    margin-top: 30px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+
+  }
+
+  .text-box {
+    background-color: #ffffff;
+    padding: 10px;
+    border-radius: 10px;
+  }
+
+  #buttonrow{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 50px;
+  }
+
+
+  .buttonswhatever a {
+    flex: 1;
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #fed9ca;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    color: #fff;
+    text-decoration: none;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    margin: 15px;
+    font-size: 14px;
+    font-weight: bold;
+    align-content: center;
+    max-width: 126px;
+    text-align: center;
+  }
+
+  .buttonswhatever a:hover {
+    background-color: #b2968b;
+  }
+
+  .bold{
+    font-weight: bold;
+  }
+
+
+  nav {
+    width: 100%;
+    font-size: 12px;
+    text-align: end;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    background-color: white;
+
+  }
+
+  nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+  }
+
+  nav {
+    text-align: center;
+    margin-left: -1rem;
+    font-size: 20px;
+    justify-content: space-around;
+    display: flex;
+    padding: 1rem 0;
+  }
+}
+@media (min-width: 1024px) {
+  .project{
+    display: none;
+  }
+  .about{
+    display: none;
+  }
   .videocontainer{
     text-align: center;
     display: flex;
@@ -134,6 +244,9 @@ import {RouterLink} from "vue-router";
     font-size: 1rem;
     padding: 1rem 0;
     margin-top: 1rem;
+  }
+  .linkedin-text {
+    display: none;
   }
 }
 </style>
